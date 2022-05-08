ff81d939d4eac40c162c264a08a7bc698a0607a5
tokai235 <the.summer.of.spiritual@gmail.com>
2022-05-08 10:23:52 +0900

    Text widgetを使ってみた

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  runApp(const Center(
+    child: Text(
+      "Hello, World!",
+      textDirection: TextDirection.ltr,
+    ),
+  ));
 }
 
 class MyApp extends StatelessWidget {
