285d734334d732b58b1727bd93e3dd55d37566cd
tokai235 <the.summer.of.spiritual@gmail.com>
2022-05-15 22:52:37 +0900

    お気に入りボタンをインタラクティブにした

--- lib/layout_tutorial.dart
+++ lib/layout_tutorial.dart
@@ -63,11 +63,7 @@ Widget titleSection = Container(
           ],
         ),
       ),
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      const Text('41'),
+      const FavoriteWidget(),
     ],
   ),
 );
@@ -116,3 +112,52 @@ Widget textSection = const Padding(
     softWrap: true,
   ),
 );
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoritedCount = 40;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoritedCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoritedCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited
+                ? const Icon(Icons.star)
+                : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Text('$_favoritedCount'),
+        ),
+      ],
+    );
+  }
+}
