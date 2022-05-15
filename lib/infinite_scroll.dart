93fb553e52a4f6549279f840c934c93628a2b539
tokai235 <the.summer.of.spiritual@gmail.com>
2022-05-15 21:05:39 +0900

    favoriteページへのナビゲーションを追加

--- lib/infinite_scroll.dart
+++ lib/infinite_scroll.dart
@@ -10,16 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Startup Name Generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Name Generator'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -36,53 +29,95 @@ class _RandomWordsState extends State<RandomWords> {
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map((pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          });
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      // itemBuilder コールバックは、単語の組み合わせの suggest ごとに1回呼び出され、
-      // 各 suggest を ListTile の行に配置します。
-      // 偶数行の場合、この関数は、単語の組み合わせのための ListTile 行を追加します。
-      // 奇数列の場合は、エントリを視覚的に分離するための Divider Widget が追加されます。
-      // なお、小さな端末では、この区切り線が見にくくなる場合があります。
-      itemBuilder: (context, i) {
-        // ListView の各行の前に、高さ 1 ピクセルの仕切り Widget を追加します。
-        if (i.isOdd) return const Divider();
-
-        // i ~/ 2 という式は i を 2 で割って整数の結果を返します。
-        // 例えば、1, 2, 3, 4, 5 は 0, 1, 1, 2, 2 になります。
-        // これは、ListView の単語ペアの実際の数を計算し、Divider Widget を除いたものです。
-        final index = i ~/ 2;
-
-        // もし、利用可能な単語の組み合わせが終了したら、
-        // さらに10個の単語を生成して候補リストに追加します。
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-
-        final alreadySaved = _saved.contains(_suggestions[index]);
-
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
           ),
-          trailing: Icon(
-            alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Colors.red : null,
-            semanticLabel: alreadySaved ? 'Removed from saved' : 'Save',
-          ),
-          onTap: () {
-            setState(() {
-              if (alreadySaved) {
-                _saved.remove(_suggestions[index]);
-              } else {
-                _saved.add(_suggestions[index]);
-              }
-            });
-          },
-        );
-      },
+        ],
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // itemBuilder コールバックは、単語の組み合わせの suggest ごとに1回呼び出され、
+        // 各 suggest を ListTile の行に配置します。
+        // 偶数行の場合、この関数は、単語の組み合わせのための ListTile 行を追加します。
+        // 奇数列の場合は、エントリを視覚的に分離するための Divider Widget が追加されます。
+        // なお、小さな端末では、この区切り線が見にくくなる場合があります。
+        itemBuilder: (context, i) {
+          // ListView の各行の前に、高さ 1 ピクセルの仕切り Widget を追加します。
+          if (i.isOdd) return const Divider();
+
+          // i ~/ 2 という式は i を 2 で割って整数の結果を返します。
+          // 例えば、1, 2, 3, 4, 5 は 0, 1, 1, 2, 2 になります。
+          // これは、ListView の単語ペアの実際の数を計算し、Divider Widget を除いたものです。
+          final index = i ~/ 2;
+
+          // もし、利用可能な単語の組み合わせが終了したら、
+          // さらに10個の単語を生成して候補リストに追加します。
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          final alreadySaved = _saved.contains(_suggestions[index]);
+
+          return ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel: alreadySaved ? 'Removed from saved' : 'Save',
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            },
+          );
+        },
+      ),
     );
   }
 }
