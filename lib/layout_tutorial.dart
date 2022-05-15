032c02597840f15dda1e28850772d81208760ca9
tokai235 <the.summer.of.spiritual@gmail.com>
2022-05-15 22:08:20 +0900

    アイコンエリアを実装

--- lib/layout_tutorial.dart
+++ lib/layout_tutorial.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
@@ -22,6 +24,7 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection(color),
           ],
         ),
       ),
@@ -61,3 +64,35 @@ Widget titleSection = Container(
     ],
   ),
 );
+
+Widget buttonSection(Color color) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      _buildButtonColumn(color, Icons.call, 'CALL'),
+      _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+      _buildButtonColumn(color, Icons.share, 'SHARE'),
+    ],
+  );
+}
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(icon, color: color),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: color,
+          ),
+        ),
+      ),
+    ],
+  );
+}
