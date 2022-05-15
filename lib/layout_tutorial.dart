2020f359dba9800266a8a23f3c7f8291774d48b0
tokai235 <the.summer.of.spiritual@gmail.com>
2022-05-15 22:37:44 +0900

    画像エリアを実装

--- lib/layout_tutorial.dart
+++ lib/layout_tutorial.dart
@@ -21,8 +21,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection(color),
             textSection,
