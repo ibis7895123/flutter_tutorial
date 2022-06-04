82f49e67f90253f081de9c9711846b53896943c2
tokai235 <the.summer.of.spiritual@gmail.com>
2022-06-04 18:39:16 +0900

    アニメーションを繰り返すようにした

--- lib/animation.dart
+++ lib/animation.dart
@@ -21,7 +21,15 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
       duration: const Duration(seconds: 2),
       vsync: this,
     );
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      })
+      ..addStatusListener((status) => print(status));
     controller.forward();
   }
 
